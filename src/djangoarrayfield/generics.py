2a7b6c1ee886cd39f5a46cba2265a853e28731b5
gawor <michal.j.gawor@gmail.com>
2023-04-13 15:47:13 +0200

    investigate Python10 vs Python11

--- src/djangoarrayfield/generics.py
+++ src/djangoarrayfield/generics.py
@@ -3,7 +3,7 @@ from typing import Protocol, TypeVar
 
 class ProtocolT(Protocol):
     def __init__(self, **kwargs):
-        self.__dict__.update(kwargs)
+        self.__dict__().update(kwargs)
 
 
 TypeT = TypeVar('TypeT', bound=ProtocolT)
